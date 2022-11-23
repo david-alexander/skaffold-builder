517ddc0d0cdd581d4ed99d10c345463aa50673ec
David Alexander <david.a@cadpro.co.nz>
2022-11-24 12:30:59 +1300

    Allow extra arguments to be passed to builder.

--- integrations/bitbucket-aks/build.sh
+++ integrations/bitbucket-aks/build.sh
@@ -2,4 +2,4 @@
 
 az login --service-principal --username $AZURE_SP_USERNAME --password $AZURE_SP_PASSWORD --tenant $AZURE_TENANT
 az aks get-credentials -g $AZURE_RESOURCE_GROUP -n $AZURE_CLUSTER --admin
-kubectl port-forward -n builder service/builder 8080:80 & curl --retry 100 --retry-delay 1 --retry-connrefused 'http://localhost:8080/job' --data-raw "https://bitbucket.org/$BITBUCKET_REPO_FULL_NAME $BITBUCKET_BRANCH $BITBUCKET_BUILD_NUMBER"
+kubectl port-forward -n builder service/builder 8080:80 & curl --retry 100 --retry-delay 1 --retry-connrefused 'http://localhost:8080/job' --data-raw "https://bitbucket.org/$BITBUCKET_REPO_FULL_NAME $BITBUCKET_BRANCH $BITBUCKET_BUILD_NUMBER $*"
